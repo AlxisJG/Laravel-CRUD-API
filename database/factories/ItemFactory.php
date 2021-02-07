77cc2d4925801c06f87b2b342ac1ce86e796a652
Alxis Javier <alxis.javier@alegra.com>
2021-02-07 12:42:29 -0400

    Se ajustan seeders

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -4,13 +4,14 @@
 
 use App\Item;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Item::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'price' => 500.48,
         'quantity' => 3,
-        'sku' => 'ABC123456',
+        'sku' => Str::random(6) . '3456',
         'description' => $faker->text
     ];
 });
